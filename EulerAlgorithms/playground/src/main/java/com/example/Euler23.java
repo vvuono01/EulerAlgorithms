804d98ac007820fcd848a1be037b6582bfc511d2
Vince Vuono <vvuono01@gmail.com>
2017-03-31 15:26:30 -0400

    Fixed problem 23

--- EulerAlgorithms/playground/src/main/java/com/example/Euler23.java
+++ EulerAlgorithms/playground/src/main/java/com/example/Euler23.java
@@ -1,60 +1,53 @@
 package com.example;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Euler23 {
-    private static final int DEFICIENT = -1;
-    private static final int PERFECT = 0;
     private static final int ABUNDANT = 1;
 
     private static final int SMALLEST_ABUNDANT_NUMBER = 12;
-    private static final int SUM_LOWER_BOUND = SMALLEST_ABUNDANT_NUMBER + SMALLEST_ABUNDANT_NUMBER;
     private static final int SUM_UPPER_BOUND = 28123;
 
     public static void main(String[] args) {
-        System.out.println("Starting...");
         System.out.println(sumOfAllIntegersThatCannotBeWrittenAsSumOfTwoAbundantNumbers());
     }
 
-    private static BigInteger sumOfAllIntegersThatCannotBeWrittenAsSumOfTwoAbundantNumbers() {
+    private static long sumOfAllIntegersThatCannotBeWrittenAsSumOfTwoAbundantNumbers() {
         List<Integer> abundantNumbers = new ArrayList<>();
-        abundantNumbers.add(SMALLEST_ABUNDANT_NUMBER);
-        for (int i = SMALLEST_ABUNDANT_NUMBER + 1; i <= SUM_UPPER_BOUND; i++) {
+        for (int i = SMALLEST_ABUNDANT_NUMBER; i <= SUM_UPPER_BOUND; i++) {
             if (numberTypeFor(i) == ABUNDANT) {
                 abundantNumbers.add(i);
             }
         }
 
-        System.out.println("Got abundant numbers");
-
-        List<Integer> unsummableNumbers = new ArrayList<>();
-        for (int i = SUM_LOWER_BOUND; i <= SUM_UPPER_BOUND; i++) {
-            unsummableNumbers.add(i);
-        }
-
-        System.out.println("Created unsummable list");
+        boolean[] unsummableNumbers = new boolean[SUM_UPPER_BOUND];
 
         for (int i = 0; i < abundantNumbers.size(); i++) {
-            for (int j = 0; j < abundantNumbers.size(); j++) {
-                Integer sum = abundantNumbers.get(i) + abundantNumbers.get(j);
-                if (unsummableNumbers.contains(sum)) {
-                    unsummableNumbers.remove(sum);
+            for (int j = i; j < abundantNumbers.size(); j++) {
+                int abundantNumberSum = abundantNumbers.get(i) + abundantNumbers.get(j);
+                if (abundantNumberSum < SUM_UPPER_BOUND) {
+                    unsummableNumbers[abundantNumberSum] = true;
                 }
             }
-            System.out.println("Through iteration " + (i+1) + "; " + (abundantNumbers.size() - (i+1)) + " iterations to go");
         }
 
-        return sum(unsummableNumbers);
+        int sum = 0;
+        for (int i = 0; i < unsummableNumbers.length; i++) {
+            if (!unsummableNumbers[i]) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 
-    private static int numberTypeFor(int n) {
-        BigInteger divisorSum = sum(properDivisors(n));
-        return divisorSum.compareTo(BigInteger.valueOf(n));
+    private static int numberTypeFor(long n) {
+        Long divisorSum = sum(properDivisors(n));
+        return divisorSum.compareTo(n);
     }
 
-    private static List<Integer> properDivisors(int n) {
+    private static List<Integer> properDivisors(long n) {
         List<Integer> divisors = new ArrayList<>();
         divisors.add(1);
         for (int i = 2; i <= (n / 2); i++) {
@@ -66,10 +59,10 @@ public class Euler23 {
         return divisors;
     }
 
-    private static BigInteger sum(List<Integer> list) {
-        BigInteger sum = BigInteger.ZERO;
+    private static long sum(List<Integer> list) {
+        long sum = 0;
         for (Integer num : list) {
-            sum = sum.add(BigInteger.valueOf(num));
+            sum += num;
         }
 
         return sum;
