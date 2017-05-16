e9c5e2feb896ebb666591cc5c99e3fd48b8d9a9c
Vince Vuono <vvuono01@gmail.com>
2017-05-16 09:54:45 -0400

    Narrowed down the search set significantly by noting the properties of some of the numbers, bringing the search time down to ~1.5s

--- EulerAlgorithms/playground/src/main/java/com/example/Euler43.java
+++ EulerAlgorithms/playground/src/main/java/com/example/Euler43.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class Euler43 {
-    private static int[] primes = {0, 1, 2, 3, 5, 7, 11, 13, 17};
+    private static int[] divisors = {0, 1, 2, 3, 5, 7, 11, 13, 17};
 
     public static void main(String[] args) {
         long startMillis = System.currentTimeMillis();
@@ -11,42 +11,43 @@ public class Euler43 {
     }
 
     private static long sumOfSubstringDivisibilityPandigitalNumbers() {
+        // http://www.mathblog.dk/project-euler-43-pandigital-numbers-sub-string-divisibility/
         long sum = 0;
-        for (int a = 1; a <= 9; a++) {
-        for (int b = 0; b <= 9; b++) {
-        for (int c = 0; c <= 9; c++) {
-        for (int d = 0; d <= 9; d++) {
-        for (int e = 0; e <= 9; e++) {
-        for (int f = 0; f <= 9; f++) {
-        for (int g = 0; g <= 9; g++) {
-        for (int h = 0; h <= 9; h++) {
-        for (int i = 0; i <= 9; i++) {
-        for (int j = 0; j <= 9; j++) {
-            if (!(a == b || a == c || a == d || a == e || a == f || a == g || a == h || a == i || a == j ||
-                  b == c || b == d || b == e || b == f || b == g || b == h || b == i || b == j ||
-                  c == d || c == e || c == f || c == g || c == h || c == i || c == j ||
-                  d == e || d == f || d == g || d == h || d == i || d == j ||
-                  e == f || e == g || e == h || e == i || e == j ||
-                  f == g || f == h || f == i || f == j ||
-                  g == h || g == i || g == j ||
-                  h == i || h == j ||
-                  i == j)) {
+        for (int d1 = 1; d1 <= 9; d1++) {
+        for (int d2 = 0; d2 <= 9; d2++) {
+        for (int d3 = 0; d3 <= 9; d3++) {
+        for (int d4 = 0; d4 <= 9; d4 += 2) {
+        for (int d5 = 0; d5 <= 9; d5 += 3) {
+        int d6 = 5;
+        for (int d7 = 0; d7 <= 9; d7++) {
+        for (int d8 = 0; d8 <= 9; d8++) {
+        for (int d9 = 0; d9 <= 9; d9++) {
+        for (int d10 = 0; d10 <= 9; d10++) {
+            if (!(d1 == d2 || d1 == d3 || d1 == d4 || d1 == d5 || d1 == d6 || d1 == d7 || d1 == d8 || d1 == d9 || d1 == d10 ||
+                  d2 == d3 || d2 == d4 || d2 == d5 || d2 == d6 || d2 == d7 || d2 == d8 || d2 == d9 || d2 == d10 ||
+                  d3 == d4 || d3 == d5 || d3 == d6 || d3 == d7 || d3 == d8 || d3 == d9 || d3 == d10 ||
+                  d4 == d5 || d4 == d6 || d4 == d7 || d4 == d8 || d4 == d9 || d4 == d10 ||
+                  d5 == d6 || d5 == d7 || d5 == d8 || d5 == d9 || d5 == d10 ||
+                  d6 == d7 || d6 == d8 || d6 == d9 || d6 == d10 ||
+                  d7 == d8 || d7 == d9 || d7 == d10 ||
+                  d8 == d9 || d8 == d10 ||
+                  d9 == d10)) {
                 StringBuilder num = new StringBuilder();
-                num.append(a);
-                num.append(b);
-                num.append(c);
-                num.append(d);
-                num.append(e);
-                num.append(f);
-                num.append(g);
-                num.append(h);
-                num.append(i);
-                num.append(j);
+                num.append(d1);
+                num.append(d2);
+                num.append(d3);
+                num.append(d4);
+                num.append(d5);
+                num.append(d6);
+                num.append(d7);
+                num.append(d8);
+                num.append(d9);
+                num.append(d10);
                 if (hasSubstringDivisibility(num.toString())) {
                     sum += Long.parseLong(num.toString());
                 }
             }
-        }}}}}}}}}}
+        }}}}}}}}}
 
         return sum;
     }
@@ -54,7 +55,7 @@ public class Euler43 {
     private static boolean hasSubstringDivisibility(String n) {
         for (int i = 2; i <= 8; i++) {
             int section = Integer.parseInt(n.substring(i-1, i+2));
-            if (section % primes[i] != 0) {
+            if (section % divisors[i] != 0) {
                 return false;
             }
         }
